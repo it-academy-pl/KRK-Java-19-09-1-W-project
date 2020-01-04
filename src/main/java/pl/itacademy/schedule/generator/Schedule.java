4531e79e46c569f9680460df1a38efc016d12168
teacher <test@example.com>
2020-01-04 12:14:13 +0100

    - lesson materials; - excel generator; - refactoring.

--- src/main/java/pl/itacademy/schedule/generator/Schedule.java
+++ src/main/java/pl/itacademy/schedule/generator/Schedule.java
@@ -1,23 +1,45 @@
 package pl.itacademy.schedule.generator;
 
-import java.time.LocalDate;
-import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
 
 public class Schedule {
 
-    private final Collection<LocalDate> lessons;
+    private final List<Lesson> lessons;
     private boolean successfulSchedule;
 
-    public Schedule(Collection<LocalDate> lessons, boolean successfulSchedule) {
+    public Schedule(List<Lesson> lessons, boolean successfulSchedule) {
         this.lessons = lessons;
         this.successfulSchedule = successfulSchedule;
     }
 
-    public Collection<LocalDate> getLessons() {
+    public List<Lesson> getLessons() {
         return lessons;
     }
 
     public boolean isSuccessfulSchedule() {
         return successfulSchedule;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Schedule schedule = (Schedule) o;
+        return successfulSchedule == schedule.successfulSchedule &&
+                Objects.equals(lessons, schedule.lessons);
+    }
+
+    @Override
+    public String toString() {
+        return "Schedule{" +
+                "lessons=" + lessons +
+                ", successfulSchedule=" + successfulSchedule +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lessons, successfulSchedule);
     }
 }
--- src/main/java/pl/itacademy/schedule/generator/ScheduleGenerator.java
+++ src/main/java/pl/itacademy/schedule/generator/ScheduleGenerator.java
@@ -5,33 +5,39 @@ import pl.itacademy.schedule.parameters.EnteredParameters;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class ScheduleGenerator implements IScheduleGenerator {
-
 
     @Override
     public Schedule generateSchedule(EnteredParameters enteredParameters) {
         LocalTime beginTime = enteredParameters.getBeginTime();
         LocalTime endTime = enteredParameters.getEndTime();
         //Period period = Period.between(endTime,beginTime);
-
+        int lessonLength = endTime.getHour() - beginTime.getHour();
         Set<DayOfWeek> lessonDays = enteredParameters.getLessonDays();
         int requiredHours = enteredParameters.getRequiredHours();
         int usedHours = 0;
         LocalDate currentDate = enteredParameters.getStartDate();
-        Collection<LocalDate> lessons = new TreeSet<>();
+        List<Lesson> lessons = new ArrayList<>();
         do {
             while (!lessonDays.contains(currentDate.getDayOfWeek())) {
                 currentDate = currentDate.plusDays(1);
-                lessons.add(currentDate);
             }
-        } while (usedHours >= requiredHours);
+            Lesson lesson = new Lesson(currentDate, beginTime, endTime);
+            lessons.add(lesson);
+            usedHours = usedHours + lessonLength;
+            currentDate = currentDate.plusDays(1);
+        } while (usedHours < requiredHours);
 
+        LocalTime lastLessonEndTime = endTime.minusHours(usedHours - requiredHours);
+        Lesson lastLesson = lessons.get(lessons.size() - 1);
+        lastLesson.setEndTime(lastLessonEndTime);
 
-        return new Schedule(lessons, true);
-
+        boolean isSuccessful = usedHours == requiredHours;
+        return new Schedule(lessons, isSuccessful);
     }
+
 }
